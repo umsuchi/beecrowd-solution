f2e3273a624d20298a15e38ab9875c91cc4c9f14
umsuchi <ummulmomeninsuchi@gmail.com>
2025-03-30 05:53:48 +0600

    Area of a Circle

---
+++ 1002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    double pi = 3.14159 , a , r;
+    cin >> r;
+
+    a = pi*r*r;
+    cout << setprecision(4) << a << endl;
+
+    return 0;
+}
