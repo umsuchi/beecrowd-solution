ff6292da94456e70e6eef967f8e01794b84330dc
umsuchi <ummulmomeninsuchi@gmail.com>
2025-03-30 20:34:40 +0600

    Average 1

--- 1005.cpp
+++ 1005.cpp
@@ -7,7 +7,7 @@ int main()
     float a , b;
     cin >> a >> b;
 
-    cout << "MEDIA = " << setprecision(5) << (a*3.5+b*7.5)/(3.5+7.5)<< endl;
+    cout << "MEDIA = " << fixed << setprecision(5) << (a*3.5+b*7.5)/(3.5+7.5)<< endl;
 
     return 0;
 }
