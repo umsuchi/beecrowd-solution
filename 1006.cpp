15e501cf1ec788a28e4c238c04bc8b44e07cfd55
umsuchi <ummulmomeninsuchi@gmail.com>
2025-03-30 20:46:14 +0600

    Average 2

---
+++ 1006.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    float a , b , c;
+    cin >> a >> b >> c;
+
+    cout << "MEDIA = " << fixed << setprecision(1) << (a*2+b*3+c*5)/(2 + 3 + 5)<< endl;
+
+    return 0;
+}
