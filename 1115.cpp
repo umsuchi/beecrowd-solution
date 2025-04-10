0ed8a7c2f1a8e32728201112ed5c19ee746d6e59
umsuchi <ummulmomeninsuchi@gmail.com>
2025-04-10 22:57:33 +0600

    Quadrant

---
+++ 1115.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x , y;
+    while(1){
+        cin >> x >> y;
+        if(x == 0 || y == 0) break;
+
+        if(x > 0 && y > 0)
+            cout << "primeiro" << endl;
+
+        else if(x > 0 && y < 0)
+            cout << "quarto" << endl;
+
+        else if(x < 0 && y < 0) 
+            cout << "terceiro" << endl;
+
+        else 
+            cout << "segundo" << endl;
+        
+    }
+
+    return 0;
+}
