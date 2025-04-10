d8c811038350711cf67ff7b96deb002244c065b1
umsuchi <ummulmomeninsuchi@gmail.com>
2025-04-10 22:18:07 +0600

    Ascending and Descending

---
+++ 1113.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    while(1){ 
+        cin >> x >> y;
+        if(x == y) break;
+        if(x > y)
+            cout << "Decrescente" << endl;
+
+        else
+            cout << "Crescente" << endl;
+    }
+    
+    return 0;
+}
